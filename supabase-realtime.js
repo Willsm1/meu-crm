ee6834fe11646bfbbfcf6d05ebec35a43c4591de
Willsm1 <willsm1@hotmail.com>
2026-09-23 14:46:01 -0300

    chore(test): bust commitment workflow cache

--- supabase-realtime.js
+++ supabase-realtime.js
@@ -36,7 +36,7 @@ function loadUiEnhancements(){
   add('script[data-tm-scope-privacy]','scope-privacy-ui.js?v=20260920-1124','tmScopePrivacy');
   add('script[data-tm-period-filters]','ui-period-filters.js?v=20260920-0242','tmPeriodFilters');
   add('script[data-tm-sales-date]','sales-date.js?v=20260920-0255','tmSalesDate');
-  add('script[data-tm-followup-commitments]','followup-commitment-ui.js?v=20260923-1415','tmFollowupCommitments');
+  add('script[data-tm-followup-commitments]','followup-commitment-ui.js?v=20260923-1416','tmFollowupCommitments');
   add('script[data-tm-followup-schedule]','followup-schedule-ui.js?v=20260920-1455','tmFollowupSchedule');
   add('script[data-tm-followup-agendar]','followup-agendar-ui.js?v=20260920-1455','tmFollowupAgendar');
   add('script[data-tm-gesture-guard]','gesture-navigation-guard.js?v=20260920-1148','tmGestureGuard');
